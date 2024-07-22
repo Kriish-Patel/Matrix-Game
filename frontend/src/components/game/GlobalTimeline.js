509ae3392ba7778e801dc75372d21ef135ddca5e
Aykan125 <zcabaak@ucl.ac.uk>
2024-07-22 14:03:20 +0100

    added scrolling for headlines

--- frontend/src/components/game/GlobalTimeline.js
+++ frontend/src/components/game/GlobalTimeline.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import socket from '../../socket';
 import '../../styles/GlobalTimeline.css';
 
-
 const GlobalTimeline = () => {
     const [acceptedHeadlines, setAcceptedHeadlines] = useState([]);
 
@@ -14,29 +13,30 @@ const GlobalTimeline = () => {
         return () => {
             socket.off('acceptedHeadline');
         };
-    }, []); 
+    }, []);
 
     return (
         <div>
-        <h1>Accepted Headlines</h1>
-        <table className="global-timeline-table">
-            <thead>
-                <tr>
-                    <th>Headline</th>
-                    <th>Year</th>
-                </tr>
-            </thead>
-            <tbody>
-                {acceptedHeadlines.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.headline}</td>
-                        <td>{item.currentYear}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-        
-    </div>
+            <h1>Accepted Headlines</h1>
+            <div className="scrollable-container">
+                <table className="global-timeline-table">
+                    <thead>
+                        <tr>
+                            <th>Headline</th>
+                            <th>Year</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {acceptedHeadlines.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.headline}</td>
+                                <td>{item.currentYear}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 };
 
