9147cf9e7a0d4739c4cf19de6649f3e0ca2a5bb0
Aykan125 <zcabaak@ucl.ac.uk>
2024-08-26 22:57:55 +0100

    theses guides are bad

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io('https://headliners-backend-2nigudq3x-aymans-projects-f9d1fc59.vercel.app/', {
   autoConnect: false, // Do not connect automatically
-  // withCredentials:true,
+  withCredentials:true,
 });
 
 
