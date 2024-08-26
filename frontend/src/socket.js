8d5dd0db0052598ee5ec5aedf23d9dbd0c91b0db
Aykan125 <zcabaak@ucl.ac.uk>
2024-08-26 23:08:35 +0100

    againa

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://headliners-backend-2nigudq3x-aymans-projects-f9d1fc59.vercel.app/', {
+const socket = io('https://headliners-backend.vercel.app/', {
   autoConnect: false, // Do not connect automatically
   withCredentials:true,
 });
