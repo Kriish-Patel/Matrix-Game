2c11dc9d4152763a3af005df83ef884599c6acc0
Aykan125 <zcabaak@ucl.ac.uk>
2024-08-27 17:14:32 +0100

    blueblocks

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -7,26 +7,6 @@ const socket = io('https://headlines-game.onrender.com', {
   withCredentials:true,
 });
 
-useEffect(() => {
-  socket.on('session', ({ sessionID, userID }) => {
-    console.log("waterfall")
-    console.log(`session socket received, sessionID: ${sessionID}`)
-    // Attach the session ID to the next reconnection attempts
-    socket.auth = { sessionID };
-    // Store it in the sessionStorage
-    sessionStorage.setItem('sessionID', sessionID);
-    // Save the ID of the user
-    socket.sessionID = sessionID;
-  });
-  const sessionID = sessionStorage.getItem('sessionID');
-  console.log(`sessionID before reconnection ${sessionID}`);
-  if (sessionID) {
-    socket.auth = { sessionID };
-    socket.connect();
-    console.log(`sessionID and we have reconnected: ${sessionID}`);
-  }
-}, []); // Empty dependency array ensures this runs only once when the app starts
-
 
 
 
